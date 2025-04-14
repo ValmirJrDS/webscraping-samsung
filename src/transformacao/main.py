27e73ce935d1271e1214541cf8d5ed4aac2b6cfe
ValmirJrDS <valmirmoreirajunior@gmail.com>
2025-04-14 15:06:49 -0300

    Incluindo arquivo requirements.txt

--- src/transformacao/main.py
+++ src/transformacao/main.py
@@ -22,8 +22,23 @@ df['reviews_rating_number'] = df['reviews_rating_number'].fillna('0')
 df['reviews_amount'] = df['reviews_amount'].fillna('0')
 
 # Garantir que estão como strings antes de usar .str
+df['old_price'] = df['old_price'].astype(str).str.replace('.', '', regex=False)
+df['new_price'] = df['new_price'].astype(str).str.replace('.', '', regex=False)
+df['reviews_amount'] = df['reviews_amount'].astype(str).str.replace('[\(\)]', '', regex=True)
 # Converter para numéricos
-# Manter apenas produtos com preço entre 1000 e 10000 reais
-# Conectar (ou criar) banco local
+df['old_price'] = df['old_price'].astype(float)
+df['new_price'] = df['new_price'].astype(float)
+df['reviews_rating_number'] = df['reviews_rating_number'].astype(float)
+df['reviews_amount'] = df['reviews_amount'].astype(int)
 # Salvar os dados em uma tabela
+# Manter apenas produtos com preço entre 1000 e 10000 reais
+df = df[
+    (df['old_price'] >= 1000) & (df['old_price'] <= 10000) &
+    (df['new_price'] >= 1000) & (df['new_price'] <= 10000)
+]
+# Conectar (ou criar) banco local
+conn = sqlite3.connect('../../data/mercadolivre.db')
+df.to_sql('notebook', conn, if_exists='replace', index=False)
+
 # Encerrar conexão
+conn.close()
